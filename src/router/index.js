172e8c507c66310fad4933c8c0c3846b58eda56e
Persolute <1538520381@qq.com>
2025-01-27 11:34:24 +0800

    基础配置

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
+
+export const constantRoutes = [{path: '/', redirect: '/bookManagement', hidden: true}, {
+    path: '/bookManagement', name: 'BookManagement', component: () => import('@/views/bookManagement')
+}]
+
+const createRouter = () => new VueRouter({
+    routes: constantRoutes, mode: 'history'
+})
+
+const router = createRouter()
+
+export default router;
---
+++ src/views/bookManagement/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="bookManagement"></div>
+</template>
+
+<script>
+export default {
+  name: "BookManagement",
+}
+</script>
+
+<style scoped>
+#bookManagement {
+  width: 100%;
+  height: 100%;
+}
+</style>
